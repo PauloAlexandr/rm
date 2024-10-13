acb732165a9b71b4f6f46ea268cf09e85a76606f
Paulo <70349878+PauloAlexandr@users.noreply.github.com>
2024-10-12 23:35:57 -0300

    limpando

--- emilly/loja.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    if (window.screen.width <= 767) {
-        var imagem = document.getElementsByClassName('slides')[0].getElementsByTagName('img');
-        imagem[0].src = 'https://cdn.awsli.com.br/2754/2754360/arquivos/fullbanner.png';
-    }
-})();
